3eecb091113fc490b5e86db2138e05a6835dda47
mainyanim <alicejarmusch@gmail.com>
2017-01-03 15:09:43 +0200

    first attempt to create a sky

--- js/minecraft.js
+++ js/minecraft.js
@@ -1,1 +1,17 @@
-myjavascript
+// var rows = 20;
+// for (var i = 0; i < rows.length; i++ ) {
+// $('br');
+//   // for (var j = 0; j < rows.length; j++) {
+//   // $('body').append("<div> id = "+j+"</div>")
+//   // $('div').addClass("sky");
+//   $('body').append('<button id = '+i+'>btn'+i+'</button>');
+//   $('#btns').append('<img src = "http://talkemoji.com/wp-content/uploads/2014/11/emoji-large-053.jpg" id = "img'+i+'">');
+// }
+
+
+for (var j = 0; j < 20; j++) {
+for (var i = 0; i < 20; i++){
+  $('body').append('<div li = '+i+'>sky'+i+'</li>');
+  $('div').addClass("sky");
+};
+}
