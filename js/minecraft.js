ac3c313c8846acac077530c4aa6edb0e514cea10
mainyanim <alicejarmusch@gmail.com>
2017-01-04 12:41:53 +0200

    added id's to every div

--- js/minecraft.js
+++ js/minecraft.js
@@ -12,17 +12,22 @@ minecraft.createSky = function () {
   });
 
   $(document).ready(function () {
-      //add columns to the the temp row object
-      for (var i = 0; i < 20; i++) {
-          var sq = $square.clone();
-          sq.attr('id','sq'+i)
-          $row.append(sq);
-      }
       //clone the temp row object with the columns to the wrapper
-      for (var i = 0; i < 9; i++) {
-          $("#wrapper").append($row.clone());
+      for (var i = 0; i < 8; i++) {
+          var row = $row.clone();
+          var rowId = 'rowSky' + i;
+          row.attr('id', rowId);
+
+          //add columns to the the temp row object
+          for (var j = 0; j < columns; j++) {
+              var sk = $square.clone();
+              sk.attr('id', rowId + '_sk' + j);
+              row.append(sk);
+          }
+
+          $("#wrapper").append(row);
       }
-    })
+  })
 } //create sky
 
 minecraft.createGrass = function () {
@@ -35,18 +40,22 @@ minecraft.createGrass = function () {
   });
 
   $(document).ready(function () {
-      //add columns to the the temp row object
-      for (var i = 0; i < 20; i++) {
-        var gr = $square.clone();
-        gr.attr('id', 'gr'+i)
-          $row.append(gr);
-
-      }
       //clone the temp row object with the columns to the wrapper
       for (var i = 0; i < 1; i++) {
-          $("#wrapper").append($row.clone());
+          var row = $row.clone();
+          var rowId = 'rowGrass' + i;
+          row.attr('id', rowId);
+
+          //add columns to the the temp row object
+          for (var j = 0; j < columns; j++) {
+              var gr = $square.clone();
+              gr.attr('id', rowId + '_gr' + j);
+              row.append(gr);
+          }
+
+          $("#wrapper").append(row);
       }
-    })
+  })
 }
 
 
@@ -60,25 +69,30 @@ minecraft.createDirt = function () {
   });
 
   $(document).ready(function () {
-      //add columns to the the temp row object
-      for (var i = 0; i < 20; i++) {
-        var dr = $square.clone();
-        dr.attr('id', 'dr'+i);
-          $row.append(dr);
-      }
       //clone the temp row object with the columns to the wrapper
       for (var i = 0; i < 4; i++) {
-          $("#wrapper").append($row.clone());
+          var row = $row.clone();
+          var rowId = 'rowdirt' + i;
+          row.attr('id', rowId);
+
+          //add columns to the the temp row object
+          for (var j = 0; j < columns; j++) {
+              var dr = $square.clone();
+              dr.attr('id', rowId + '_dr' + j);
+              row.append(dr);
+          }
+
+          $("#wrapper").append(row);
       }
-    })
+  })
 }
 
 minecraft.createMatrix = function () {
 minecraft.matrix = new Array();
-minecraft.skyArr = new Array(200);
+minecraft.skyArr = new Array(160);
 minecraft.skyArr[0] = "c";
 minecraft.grassArr = new Array(20);
-minecraft.dirtArr = new Array (100);
+minecraft.dirtArr = new Array (80);
 minecraft.matrix.push(minecraft.skyArr,minecraft.grassArr, minecraft.dirtArr);
 }
 
